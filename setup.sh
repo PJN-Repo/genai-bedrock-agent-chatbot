aab3b69c7d55b929c335fb2e839ff0a5bf6af4a4
Matt <matthew.monson@gmail.com>
2024-10-09 13:51:20 +0000

    gitpod.

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ source .venv/bin/activate
 python3 -m pip install -r requirements.txt
 npm install -g aws-cdk@latest
 apt-get update && apt-get install -y zip
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && ./aws/install && aws --version
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip 
+sudo ./aws/install && aws --version
 export LAMBDA_FUNCTION_NAME=invokeAgentLambda
